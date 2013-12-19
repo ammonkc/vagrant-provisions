8bb010a8cf86fdcd0713d838d47f66115dc84b11
Ammon Casey <ammon@caseyohana.com>
2013-12-19 09:06:50 -1000

    initial commit

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+# xterm colors
+if [[ $COLORTERM = gnome-* && $TERM = xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
+    export TERM=gnome-256color
+elif infocmp xterm-256color >/dev/null 2>&1; then
+    export TERM=xterm-256color
+fi
+
+# Notice title
+notice() { printf  "\033[1;32m=> $1\033[0m \n"; }
+# Error title
+error() { printf "\033[1;31m=> Error: $1\033[0m \n"; }
+# List item
+c_list() { printf  "  \033[1;32m✔\033[0m $1 \n"; }
+# Error list item
+e_list() { printf  "  \033[1;31m✖\033[0m $1 \n"; }
+
+if [ "$EUID" -ne "0" ] ; then
+    error "Script must be run as root." >&2
+    exit 1
+fi
+
+install_puppet() {
+    notice "Installing Puppet repo for CentOS-6.5-x86_64"
+    wget -qO /tmp/puppetlabs-release-6-7.noarch.rpm \
+    http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+
+    rpm -ivh /tmp/puppetlabs-release-6-7.noarch.rpm
+    rm /tmp/puppetlabs-release-6-7.noarch.rpm
+    yum update
+    notice "Installing puppet"
+    yum install -y puppet facter
+    notice "Puppet installed!"
+}
+
+install_puppet_modules() {
+    notice "*************************"
+    notice "**      Bootstrap      **"
+    notice "*************************"
+    notice "Install puppet modules"
+    pmod_path="/etc/puppet/modules"
+    vpmod_path="/tmp/vagrant-puppet-1/modules-0"
+    puppet_bin="/usr/bin/puppet"
+    git_bin="/usr/bin/git"
+    pmods=( $(ls $pmod_path) )
+    if [ -d $vpmod_path ]; then
+        vpmods=( $(ls $vpmod_path) )
+        for index in "${!pmod_deps[@]}"; do
+            not_in_array ${pmod_deps[$index]} "${vpmods[@]}"
+            missing_vpmod=$?
+            if [ $missing_vpmod -gt 0 ]; then
+                not_in_array ${pmod_deps[$index]} "${pmods[@]}"
+                should_install=$?
+                if [ $should_install -gt 0 ]; then
+                    e_list "${pmod_deps[$index]} ==> ${mod_repos[$index]}"
+                    if [ "${pmod_deps[$index]%%-*}" == "ammonkc" ]; then
+                        $git_bin clone ${mod_repos[$index]} "${pmod_path}/${pmod_deps[$index]##*-}"
+                    else
+                        $puppet_bin module install ${pmod_deps[$index]} --modulepath ${pmod_path}
+                    fi
+                else
+                    c_list "${pmod_deps[$index]} ==> ${pmod_path}"
+                fi
+            else
+                c_list "${pmod_deps[$index]} ==> ${vpmod_path}"
+            fi
+        done
+    else
+        for index in "${!pmod_deps[@]}"; do
+            not_in_array ${pmod_deps[$index]} "${pmods[@]}"
+            should_install=$?
+            if [ $should_install -gt 0 ]; then
+                e_list "${pmod_deps[$index]} ==> ${mod_repos[$index]}"
+                if [ "${pmod_deps[$index]%%-*}" == "ammonkc" ]; then
+                    $git_bin clone ${mod_repos[$index]} "${pmod_path}/${pmod_deps[$index]##*-}"
+                else
+                    $puppet_bin module install ${pmod_deps[$index]} --modulepath ${pmod_path}
+                fi
+            else
+                c_list "${pmod_deps[$index]} ==> ${pmod_path}"
+            fi
+        done
+    fi
+}
+
+not_in_array() {
+  local needle=$1
+  local hay=$2
+  shift
+  for hay; do
+    [[ $hay == ${needle##*-} ]] && return 0
+  done
+  return 1
+}
+
+#-----------------------------------------------------------------------------
+# Array data
+#-----------------------------------------------------------------------------
+
+pmod_deps=(             \
+    ammonkc-repo        \
+    ammonkc-pkg         \
+    ammonkc-server      \
+    example42-apache    \
+    puppetlabs-mysql    \
+    puppetlabs-firewall \
+    example42-php       \
+    maestrodev-wget     \
+)
+mod_repos=(                                                 \
+    https://github.com/ammonkc/puppet-repo.git              \
+    https://github.com/ammonkc/puppet-pkg.git               \
+    https://github.com/ammonkc/puppet-server.git            \
+    https://github.com/example42/puppet-apache.git          \
+    https://github.com/puppetlabs/puppetlabs-mysql.git      \
+    https://github.com/puppetlabs/puppetlabs-firewall.git   \
+    https://github.com/example42/puppet-php.git             \
+    https://github.com/maestrodev/puppet-wget.git           \
+)
+#-----------------------------------------------------------------------------
+# Initialize
+#-----------------------------------------------------------------------------
+
+if hash puppet 2> /dev/null ; then
+    notice "Puppet is installed"
+else
+    install_puppet
+fi
+
+install_puppet_modules
---
+++ manifests/default.pp
@@ -0,0 +1,70 @@
+#------------------------------------------
+# Vagrant Dev VM
+#------------------------------------------
+# - Base set up
+# - Epel repo
+# - basic utility packages
+#
+#------------------------------------------
+
+# Set the default $PATH$ for executing commands on node systems.
+Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:" }
+
+# Run Stages
+stage { 'pre':  before  => Stage['main'] }
+stage { 'post': require => Stage['main'] }
+
+# Default node
+node default {
+    file { 'motd':
+        ensure  => file,
+        path    => '/etc/motd',
+        mode    => 0644,
+        content => "
+        *****************************
+        **     Vagrant Dev VM      **
+        *****************************
+            - Version:  0.0.6
+            - OS:       CentOS-6.5_x86_64
+            - Box:      CentOS-6.5
+            - Hostname: ${fqdn}
+            - IP:       ${ipaddress_eth1}
+            - User:     vagrant@${fqdn}
+            - Password: vagrant
+            - Code:     /vagrant
+\n"
+    }
+}
+
+# register run stages
+class { 'repo::epel': stage => pre; }
+
+#------------------------------------------
+# Utils
+#------------------------------------------
+if $utils == 'true' {
+    info("Installing Utilities")
+    # install utilities
+    include pkg::utils
+}
+
+#------------------------------------------
+# LAMP stack - Apache/MySQL/PHP
+#------------------------------------------
+if $puppet_node =~ /^lamp.([0-9a-zA-Z]*-?[0-9a-zA-Z]*)([0-9a-zA-Z]*_?[0-9a-zA-Z]*)\.vagrant.dev$/ {
+    include server::lamp
+}
+
+#------------------------------------------
+# LEMP stack - Nginx/MySQL/PHP
+#------------------------------------------
+if $puppet_node =~ /^lemp.([0-9a-zA-Z]*-?[0-9a-zA-Z]*)([0-9a-zA-Z]*_?[0-9a-zA-Z]*)\.vagrant.dev$/ {
+    include server::lemp
+}
+
+#------------------------------------------
+# Node.js
+#------------------------------------------
+if $nodejs == 'true' {
+    include node-js
+}
