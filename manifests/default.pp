a0b59cdd495f93d86e8edb4a601850b2fcd60a45
Ammon Casey <ammon@caseyohana.com>
2013-12-19 12:56:18 -1000

    fixing dotfiles

--- manifests/default.pp
+++ manifests/default.pp
@@ -60,7 +60,7 @@ if $dotfiles == 'true' {
       shell  => "/bin/zsh",
     }
     exec { "dotfiles":
-        command => "bash < <( curl https://raw.github.com/ammonkc/dotfiles/linux/bootstrap.sh ) 2&>/dev/null",
+        command => "bash < <( curl https://raw.github.com/ammonkc/dotfiles/linux/bootstrap.sh )",
         user    => "vagrant",
         path    => "/bin/",
     }
